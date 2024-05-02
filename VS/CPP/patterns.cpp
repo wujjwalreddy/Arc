33d9d6ce87fab5478cc2d1303f1ead07ed4a4933
wujjwalreddy <147243888+wujjwalreddy@users.noreply.github.com>
2024-05-02 17:45:59 +0530

    Add files via upload

---
+++ VS/CPP/patterns.cpp
@@ -0,0 +1,49 @@
+// C++ code to demonstrate star pattern using recursion
+#include <iostream>
+using namespace std;
+
+// recursive function to print * on console
+void print(int n)
+{
+	if(n == 0) // base case
+	{
+		return;
+	}
+	cout << "* "; 
+	print(n - 1); // calling recursively
+}
+
+// recursive function to demonstrate printing pattern
+void pattern(int n)
+{
+	// base case
+	if (n == 0) 
+	{
+		return;
+	}
+	else
+	{
+		// calling function recursively
+		pattern(n - 1);
+
+		// print "* " n times
+		print(n);
+	
+		// Ending line after each row
+		cout << endl;
+	}
+}
+
+// Driver Function
+int main()
+{
+	int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        pattern(n);
+        cout<<"\n";
+    }
+}
+// this code is contributed by Shivesh Kumar Dwivedi
